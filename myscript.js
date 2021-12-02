0e2cd93efb911f28c192051e1c5f4cbb35c9b378
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2021-12-02 15:29:53 +0100

    progetto finito

---
+++ myscript.js
@@ -0,0 +1,5 @@
+let userInputName = prompt('inserisci il tuo nome')
+let userInputSurname = prompt('inserisci il tuo cognome')
+let userInputColor = prompt('inserisci il tuo colore preferito')
+
+document.getElementById('mj_password').innerHTML = userInputName + userInputSurname + userInputColor + 21
